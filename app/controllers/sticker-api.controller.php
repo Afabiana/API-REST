e050fad958720f5c171714ff7a5bf39a308ffb94
Fabiana Augusto <fabianaaugusto018@gmail.com>
2022-11-13 02:43:56 -0300

    anda todo (o casi) FALTA ORDENAR ESTE DESQUICIO

--- app/controllers/sticker-api.controller.php
+++ app/controllers/sticker-api.controller.php
@@ -22,13 +22,15 @@ class StickerApiController{
 
     public function getStickers($params = null){
         //declaro valores por default
-        $page=1;
         $sort="numero";
-        $filter="numero=0";
+        $filter="numero=0"; //no hay resultados que respondan a esa busqueda
         $page=1;
         $order="asc";
         $limit=$this->model->getTableSize();//esta funcion retorna el total de filas que tengo
         
+        //si limit es mayor al numero de los resultados deberia decirlo?
+        //verificar que para cambiar el valor de order, $_GET no esta vacio
+        //si recibo parametros por get cambio el valor de las variables
         if(isset($_GET['sort'])&&!empty($_GET['sort'])&&$this->isColumn($_GET['sort']))
             $sort=$_GET['sort'];
         if(isset($_GET['filter'])&&!empty($_GET['filter']))
@@ -37,14 +39,13 @@ class StickerApiController{
             $page=$_GET['page'];
         if(isset($_GET['limit'])&&!empty($_GET['limit'])&&is_numeric($_GET['limit']))
             $limit=$_GET['limit']; 
-        if(isset($_GET['order'])&&!empty($_GET['order'])&&($_GET['order']!="asc"||$_GET['order']!="desc"))
+        if(isset($_GET['order'])&&!empty($_GET['order'])&&($_GET['order']=="asc"||$_GET['order']=="desc"))
             $order=$_GET['order'];
 
         $this->paginate($limit, $page, $order, $sort, $filter);
     }
 
     public function paginate($limit, $page, $order, $sort, $filter){
-
         //hago un explode del filtro. Tengo nombre columna por un lado, y valor por el otro
         $filter=preg_split('/[=|&|?]/', $filter);
         if($this->isColumn($filter[0])){
@@ -52,6 +53,7 @@ class StickerApiController{
             $value=$filter[1];
         }else{
             $this->view->response("Esa columna no existe", 404);
+            die();
         }
         //evaluo variable page y limit para evitar inyeccion sql
         //calcular de manera que se pueda mostrar que ese numero de pagina no existe
@@ -70,11 +72,13 @@ class StickerApiController{
         $column=1;
         $value=0;
         $stickers=$this->model->getStickersByUser($column, $value, $user);
+
         if($stickers)
             $this->view->response($stickers);
         else 
             $this->view->response("No hay figuritas con ese status", 404);
     }
+
     public function getSticker($params = null){
         var_dump($params);
         //traigo el id del arreglo de params
@@ -96,7 +100,6 @@ class StickerApiController{
         }
 
         $sticker = $this->getData();
-        var_dump($params);
         if(empty($sticker->numero)||empty($sticker->nombre)||empty($sticker->apellido)||empty($sticker->id_pais)){
             $this->view->response("Complete los datos", 400);
         }else{ 
@@ -134,16 +137,10 @@ class StickerApiController{
 
         if($sticker){
             $this->model->update($id, $data->nombre, $data->apellido,$data->id_pais);
-        $this->view->response("La figurita $id se modifico con exito:");
+            $this->view->response("La figurita $id se modifico con exito:");
         //podria armar el json como string y mandarlo todo como un mismo texto
         }else
             $this->view->response("La figurita=$id no existe",404);
-    }
-
-    private function getOrdered($sort, $order){
-        //del model me traigo la lista de figuritas ORDENADAS
-        $ordered= $this->model->order($order,$sort);
-        $this->view->response($ordered);
     }
 
     private function isColumn($newcol){
